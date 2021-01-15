b6e7615fa2be3ffde822d6028ca4e5a5375923ef
Ivan Jovanović <mrtanguero@gmail.com>
2021-01-15 20:03:05 +0100

    Dodat jedan await i ispravljena poređenja, uklonjen neki višak

--- src/App.js
+++ src/App.js
@@ -1,137 +1,122 @@
 import React, { Component } from 'react';
 import GameContainer from './components/GameContainer';
-import ControlPanel from './components/ControlPanel'
+import ControlPanel from './components/ControlPanel';
 
-const imgID = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]
+const imgID = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
 
-function shuffle (array) {
+function shuffle(array) {
   let counter = array.length;
 
-   while (counter > 0) {
-      
-      let index = Math.floor(Math.random() * counter);
+  while (counter > 0) {
+    let index = Math.floor(Math.random() * counter);
 
-      counter--;
+    counter--;
 
-      let temp = array[counter];
-      array[counter] = array[index];
-      array[index] = temp;
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
   }
 
   return array;
 }
 
 class App extends Component {
-
-  state={
+  state = {
     imgNum: shuffle(imgID),
     flipedCard: false,
     firstCard: null,
     secondCard: null,
     lockUntilFlip: false,
-    lockFlip: false, 
-    playerMoves: 5, 
-    points: 0
-
-  }
+    lockFlip: false,
+    playerMoves: 5,
+    points: 0,
+  };
 
   handleReset = () => {
-    return(
-      this.setState({
-        imgNum: shuffle(imgID),
-        flipedCard: false,
-        firstCard: null,
-        secondCard: null,
-        lockUntilFlip: false,
-        lockFlip: false, 
-        playerMoves: 5, 
-        points: 0
-      })
-    )
-  }
+    return this.setState({
+      imgNum: shuffle(imgID),
+      flipedCard: false,
+      firstCard: null,
+      secondCard: null,
+      lockUntilFlip: false,
+      lockFlip: false,
+      playerMoves: 5,
+      points: 0,
+    });
+  };
 
   cardFlip = async (e) => {
+    let flipedCard = this.state.flipedCard;
+    let lockUntilFlip = this.state.lockUntilFlip;
+    let firstCard = this.state.firstCard;
+    let targetName = e.target.parentNode;
 
-    let flipedCard = this.state.flipedCard
-    let lockUntilFlip = this.state.lockUntilFlip
-    let firstCard = this.state.firstCard
-    let targetName = e.target.parentNode
-
-    if(lockUntilFlip) return;
-    if(targetName === firstCard) return;
+    if (lockUntilFlip) return;
+    if (targetName === firstCard) return;
     targetName.classList.toggle('flip');
 
-    if(!flipedCard){
+    if (!flipedCard) {
       // Igrač prvi put klikce na kartu
       // Setuje se u state da je flip registrovan na true
-      this.setState(() => {return ({ flipedCard: true})})
-      this.setState(() => {return ({ firstCard: targetName})})
-
-      // this.setState({ 
-      //   flipedCard: true , 
-      //   firstCard: targetName ,
-      // })
+      this.setState({
+        flipedCard: true,
+        firstCard: targetName,
+      });
       return;
+    } else {
+      // Ako je igrač već kliknuo
+      //! OVAJ AWAIT MORA
+      await this.setState({ flipedCard: false, secondCard: targetName });
     }
-
-    // Ako je igrač već kliknuo
-    this.setState({ flipedCard: false, secondCard: targetName })
 
     // Provjera podudarnosti dvije kliknute karte
     this.checkMatch();
-  }
+  };
 
-  checkMatch = async () => {
-    const firstCard = this.state.firstCard
-    const secondCard = this.state.secondCard
-    const points = this.state.points
-    const moves = this.state.playerMoves
-    if(firstCard === secondCard){
+  checkMatch = () => {
+    const firstCard = this.state.firstCard;
+    const secondCard = this.state.secondCard;
+    const points = this.state.points;
+    const moves = this.state.playerMoves;
+    if (firstCard.dataset.name === secondCard.dataset.name) {
       // console.log(this.state.lockFlip)
-      return (
-        this.setState(() => {return({ points:( points + 1)})}),
-        this.setState(() => {return({ lockFlip: true})})
-
-        // this.setState({ points: points + 1, lockFlip: true })
-        // console.log(this.state.lockFlip)
-      )
+      this.setState({ points: points + 1, lockFlip: true }, () => {
+        return;
+      });
     }
-    if(firstCard !== secondCard){
-      return (
-        this.setState(() => {return({ playerMoves: (moves - 1) })}),
-        this.flipAgain()
-      )
+    if (firstCard.dataset.name !== secondCard.dataset.name) {
+      this.setState({ playerMoves: moves - 1 });
+      this.flipAgain();
+      return;
     }
-  }
+  };
 
   flipAgain = () => {
-    this.setState(() => {return({ lockUntilFlip: true })});
+    this.setState({ lockUntilFlip: true });
     setTimeout(() => {
-      this.state.firstCard.classList.remove('flip')
-      this.state.secondCard.classList.remove('flip')
-      this.setState(() => {return ({ lockUntilFlip: false})})
-    }, 1500)
-  }
+      this.state.firstCard.classList.remove('flip');
+      this.state.secondCard.classList.remove('flip');
+      this.setState({ lockUntilFlip: false });
+    }, 1500);
+  };
 
-  render() { 
-    return ( 
+  render() {
+    return (
       <div className="container">
-        <ControlPanel 
-          playerMoves={ this.state.playerMoves }
-          points={ this.state.points }
-          handleReset={ this.handelReset }
-
+        <ControlPanel
+          playerMoves={this.state.playerMoves}
+          points={this.state.points}
+          handleReset={this.handelReset}
         />
 
         <GameContainer
-          imgNum={ this.state.imgNum}
-          cardFlip={ this.cardFlip }
-          lockFlip={ this.state.lockFlip }
+          imgNum={this.state.imgNum}
+          cardFlip={this.cardFlip}
+          lockFlip={this.state.lockFlip}
         />
-
       </div>
-     );
+    );
   }
 }
- 
+
 export default App;
